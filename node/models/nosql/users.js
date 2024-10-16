336dc77f357b1aa636782a7274b0d5a72440bfbc
Iker Solozabal <ikersolozabal4@gmail.com>
2024-10-16 17:53:32 +0200

    feat(models): add Mongoose schema and model for users

---
+++ node/models/nosql/users.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+    {
+        email: {
+            type:String,
+            unique:true
+        },
+        password:{
+            type:String
+        },
+        role:{
+            type:["user", "admin"],
+            default:"user"
+        }
+    },
+    {
+        timestamps:true,
+        versionKey:false
+    }
+)
+
+module.exports = mongoose.model("users", UserSchema)
