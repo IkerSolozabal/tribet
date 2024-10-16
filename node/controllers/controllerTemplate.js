7f5f7f7f33c6c4c3890d3698909b71e0a7304de0
Iker Solozabal <ikersolozabal4@gmail.com>
2024-10-16 23:02:59 +0200

    chore(controllers): add controller template

---
+++ node/controllers/controllerTemplate.js
@@ -0,0 +1,17 @@
+// get a list of all items.
+const getItems = (req, res) => {};
+
+// get a single item by its ID or a unique identifier.
+const getItem = (req, res) => {};
+
+// create a new item.
+const createItem = (req, res) => {};
+
+// delete an existing item.
+const deleteItem = (req, res) => {};
+
+// update an existing item.
+const updateItem = (req, res) => {};
+
+
+module.exports = {getItems, getItem, createItem, updateItem, deleteItem}
