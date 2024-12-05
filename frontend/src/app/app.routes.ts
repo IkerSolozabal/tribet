43aa125a02b5c044580163e66c35eb1ddfb63aec
Iker Solozabal <ikersolozabal4@gmail.com>
2024-12-06 00:04:28 +0100

    chore: format

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,37 +1,37 @@
-import { Routes } from '@angular/router';
-import { LoginFormComponent } from './components/login-form/login-form.component';
-import { LandingComponent } from './components/landing/landing.component';
-import { AuthGuard } from './shared/guards/auth/auth.guard';
-import { EventPageComponent } from './components/events/event-page/event-page.component';
-import { BetPageComponent } from './components/bets/bet-page/bet-page.component';
-import { UserBetsPageComponent } from './components/mybets/user-bets-page/user-bets-page.component';
+import {Routes} from '@angular/router';
+import {LoginFormComponent} from './components/login-form/login-form.component';
+import {LandingComponent} from './components/landing/landing.component';
+import {AuthGuard} from './shared/guards/auth/auth.guard';
+import {EventPageComponent} from './components/events/event-page/event-page.component';
+import {BetPageComponent} from './components/bets/bet-page/bet-page.component';
+import {UserBetsPageComponent} from './components/mybets/user-bets-page/user-bets-page.component';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: LoginFormComponent
-    },
-    {
-        path: 'login',
-        component: LoginFormComponent
-    },
-    {
-        path: 'home',
-        component: LandingComponent
-    },
-    {
-        path: 'events',
-        component: EventPageComponent,
-        canActivate: [AuthGuard]
-    },
-    {
-        path: 'mybets',
-        component: UserBetsPageComponent,
-        canActivate: [AuthGuard]
-    },
-    {
-        path: 'events/:eventId',
-        component: BetPageComponent,
-        canActivate: [AuthGuard]
-    }
+  {
+    path: '',
+    component: LoginFormComponent
+  },
+  {
+    path: 'login',
+    component: LoginFormComponent
+  },
+  {
+    path: 'home',
+    component: LandingComponent
+  },
+  {
+    path: 'events',
+    component: EventPageComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'mybets',
+    component: UserBetsPageComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'events/:eventId',
+    component: BetPageComponent,
+    canActivate: [AuthGuard]
+  }
 ];
