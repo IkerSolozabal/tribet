8315e8961a86af5df1586fd1c3c3ed948457e9b8
Iker Solozabal <ikersolozabal4@gmail.com>
2025-03-01 12:36:33 +0100

    feat(auth): bypass authentication and authorization in pre environment for testing
    Skip authentication and authorization checks when `NODE_ENV` is set to `pre` to facilitate testing.
    Applied changes in both `role.js` and `session.js`.

--- backend/middlewares/role.js
+++ backend/middlewares/role.js
@@ -1,22 +1,27 @@
 const {handleHttpError} = require("../utils/handleError");
-
+require("dotenv").config()
+const environment = process.env.NODE_ENV
 const checkRole = (permittedRoles) => (req, res, next) => {
-    try {
-        const {user} = req;
-
-        if (!user || !user.role) {
-            return handleHttpError(res, "USER_NOT_AUTHORIZED", 401);
-        }
-
-        const hasPermission = permittedRoles.includes(user.role);
-
-        if (!hasPermission) {
-            return handleHttpError(res, "USER_NOT_PERMISSIONS", 403);
-        }
-
+    if (environment == "pre") {
         next();
-    } catch (error) {
-        handleHttpError(res, "ERROR_PERMISSIONS", 500, error);
+    } else {
+        try {
+            const {user} = req;
+
+            if (!user || !user.role) {
+                return handleHttpError(res, "USER_NOT_AUTHORIZED", 401);
+            }
+
+            const hasPermission = permittedRoles.includes(user.role);
+
+            if (!hasPermission) {
+                return handleHttpError(res, "USER_NOT_PERMISSIONS", 403);
+            }
+
+            next();
+        } catch (error) {
+            handleHttpError(res, "ERROR_PERMISSIONS", 500, error);
+        }
     }
 };
 
